759e5f9a80a21971384cb830c23d1746567e18c0
Ming <zhaomingming455@163.com>
2021-09-01 06:56:35 +0800

    update less

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -258,91 +258,14 @@ export default {
 
 }
 
-.icon {
-  display: inline-block;
-  padding: 2px;
-  font-size: 12px;
-  // background: #eee;
-  cursor: pointer;
-  margin-right: 4px;
-}
-
-.motion-common() {
-    animation-duration: .3s;
-    animation-fill-mode: both;
-}
-
-.move-motion(@className, @keyframeName) {
-    .make-motion(@className, @keyframeName);
-    .@{className}-enter-active {
-        animation-timing-function: ease-in-out;
-    }
-    .@{className}-leave-active {
-        animation-timing-function: ease-in-out;
-    }
-}
-
-.make-motion(@className, @keyframeName) {
-    .@{className}-enter-active {
-        .motion-common();
-        animation-play-state: paused;
-    }
-    .@{className}-leave-active {
-        .motion-common();
-        animation-play-state: paused;
-    }
-    .@{className}-enter-active {
-        animation-name: ~"@{keyframeName}In";
-        animation-play-state: running;
-    }
-    .@{className}-leave-active {
-        animation-name: ~"@{keyframeName}Out";
-        animation-play-state: running;
-    }
-}
-
-.move-motion(transition-drop, shTransitionDrop);
-
-@keyframes shTransitionDropIn {
-    0% {
-        opacity: 0;
-        transform: scaleY(0.8);
-    }
-    100% {
-        opacity: 1;
-        transform: scaleY(1);
-    }
-}
-
-@keyframes shTransitionDropOut {
-    0% {
-        opacity: 1;
-        transform: scaleY(1);
-    }
-    100% {
-        opacity: 0;
-        transform: scaleY(0.8);
-    }
-}
 
 
 
-.rotate-motion(@className, @deg) {
-  .transform {
-    transform-origin: center;
-    transition: @transition-time;
-  }
-  .@{className}-enter {
-    transform: rotate(@deg);
-    .transform;
-  }
-  .@{className}-leave {
-    transform: rotate(0deg);
-    .transform;
-  }
-}
-.rotate-motion(rotate90, 90deg);
-.rotate-motion(rotate180, 180deg);
+
+
+
+
+
 .el-tree__drop-indicator {
     position: absolute;
     left: 0;
@@ -353,117 +276,8 @@ export default {
 
 
 
-.tree {
-  position: relative;
-  .tree-node {
-      padding: 4px 0;
-     .tree-node-name {
-      display: inline-block;
-    }
-  }
-  .is-drop-inner > .tree-node-content > .tree-node-name {
-    background-color: #409eff;
-    color: #fff;
-  }
-}
 
-.vue-tree {
-  user-select: none;
-  position: relative;
-  .drop-indicator {
-    position: absolute;
-    left: 0;
-    right: 0;
-    height: 1px;
-    background-color: @primary-color;
-  }
-  .child-node {
-    padding-left: 22px;
-    line-height: 28px;
-    cursor: pointer;
-    .node-content {
-      width: 100%;
-      position: relative;
-      display: flex;
-      padding-left: 22px;
-      .icon {
-        position: absolute;
-        display: inline-block;
-        left: 0px;
-        &-expand {
-          color: @normal-color;
-        }
-        &-unexpand {
-          color: @unimportance-color;
-        }
-      }
-      .inner-wrap {
-        padding-left: 2px;
-        display: flex;
-        align-items: center;
-        border-radius: @border-radius-small;
-        border: 1px solid transparent;
-      }
-      .drop-wrap {
-        border-color: @primary-color;
-      }
-      .drop-inner {
-        border-bottom: 1px solid @primary-color;
-      }
-    }
-    .active-li {
-      .inner-wrap {
-        background: @background-color-base;
-      }
-    }
-    .inset {
-      cursor: move;
-    }
-    .disabled {
-      cursor: no-drop;
-    }
-  }
-  .add-node {
-    cursor: pointer;
-  }
-.is-drop-inner > .node-content > .inner-wrap > .node-name {
-  background-color: @primary-color;
-  color: #fff;
-}
-}
-.vue-checkbox {
-  position: relative;
-  width: 13px;
-  height: 13px;
-  &-input {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    z-index: 3;
-    background: transparent
-  }
-  &-indeterminate {
-    box-sizing: border-box;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    background: #0075ff;
-    z-index: 4;
-    border-radius: 3px;
-    &:after {
-      position: absolute;
-      content: '';
-      width: 100%;
-      height: 2px;
-      background: #fff;;
-      top: 50%;
-      left: 0;
-    }
-  }
-}
+
+
 
 </style>
